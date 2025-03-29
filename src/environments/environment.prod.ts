6a214b926d8cca613306970506a9ebc24c1c3c42
Ruben Martinez <rubemm18@gmail.com>
2025-03-29 16:21:07 -0400

    Ajuste para ambientes de dev y prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://app.apollogroupsport.com/api',
+    // urlFrontend: 'https://tudominio.com'
+    // Aquí puedes añadir más variables específicas para producción
+};
